5ae424e4782c333825c52f88fda47018701ad474
dszz4r4gdt-droid <dszz4r4gdt@privaterelay.appleid.com>
2026-07-30 14:28:46 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,9 +14,27 @@ bot = commands.Bot(
 @bot.event
 async def on_ready():
     print(f"{bot.user} is online!")
+    print("Commands:", [command.name for command in bot.commands])
+
 
 @bot.command()
 async def testbot(ctx):
     await ctx.send("Mon bot fonctionne !")
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong !")
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Salut {ctx.author.mention} !")
+
+
+@bot.command()
+async def info(ctx):
+    await ctx.send("Je suis un bot Discord hébergé sur Railway.")
+
 
 bot.run(TOKEN)
